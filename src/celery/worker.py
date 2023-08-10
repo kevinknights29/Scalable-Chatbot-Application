54869db0229c11af5767792f520eaa47af4ad3ad
Kevin Knights <kevinknights29@hotmail.com>
2023-08-09 23:19:04 -0500

    WIP: getting celery service to work

--- src/celery/worker.py
+++ src/celery/worker.py
@@ -6,6 +6,9 @@ from celery import signals
 from src.models.loader import ModelLoader
 from src.utils.utils import generate_output
 
+model_loader = None
+model_path = "meta-llama/Llama-2-7b-chat-hf"
+
 
 def make_celery(app_name=__name__):
     backend = broker = "redis://llama2_redis_1:6379/0"
@@ -13,9 +16,6 @@ def make_celery(app_name=__name__):
 
 
 celery = make_celery()
-
-model_loader = None
-model_path = "meta-llama/Llama-2-7b-chat-hf"
 
 
 @signals.worker_process_init.connect
@@ -32,3 +32,7 @@ def generate_text_task(prompt):
         model_loader.tokenizer,
     )
     return model_loader.tokenizer.decode(outputs[0]), time, memory
+
+
+if __name__ == "__main__":
+    celery.start()
