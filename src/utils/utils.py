53be53decfe0854dba39a2f964dfa417a730e642
Kevin Knights <kevinknights29@hotmail.com>
2023-09-01 16:35:56 -0500

    updated attributes to run models on cpu

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import functools
 import time
 
+import psutil
 import torch
 from transformers import AutoModelForCausalLM
 from transformers import AutoTokenizer
@@ -23,12 +24,19 @@ def time_decorator(func):
 def memory_decorator(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        torch.cuda.empty_cache()
-        torch.cuda.reset_peak_memory_stats()
-        result, exec_time = func(*args, **kwargs)
-        peak_mem = torch.cuda.max_memory_allocated()
-        peak_mem_consumption = peak_mem / 1e9
-        return peak_mem_consumption, exec_time, result
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+            torch.cuda.reset_peak_memory_stats()
+            result, exec_time = func(*args, **kwargs)
+            peak_mem = torch.cuda.max_memory_allocated()
+            peak_mem_consumption = peak_mem / 1e9
+            return peak_mem_consumption, exec_time, result
+        else:
+            peak_mem_start = psutil.virtual_memory().peak_wset / (1024**3)
+            result, exec_time = func(*args, **kwargs)
+            peak_mem_end = psutil.virtual_memory().peak_wset / (1024**3)
+            peak_mem_consumption = peak_mem_end - peak_mem_start
+            return peak_mem_consumption, exec_time, result
 
     return wrapper
 
@@ -41,6 +49,6 @@ def generate_output(
     tokenizer: AutoTokenizer,
 ) -> torch.Tensor:
     input_ids = tokenizer(prompt, return_tensors="pt").input_ids
-    input_ids = input_ids.to("cuda")
+    input_ids = input_ids.to("cuda:0" if torch.cuda.is_available() else "cpu")
     outputs = model.generate(input_ids, max_length=500)
     return outputs
